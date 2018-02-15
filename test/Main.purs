a62064ef649da5e6760277b19a13b1753f5f8847
kritzcreek <christoph.hegemann1337@gmail.com>
2018-02-15 16:37:07 +0100

    Moves back to using a List of AVars for the listeners

--- test/Main.purs
+++ test/Main.purs
@@ -1,12 +1,12 @@
 {-
-Copyright 2016 SlamData, Inc.
+Copyright 2018 SlamData, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
-    
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,7 +17,8 @@ limitations under the License.
 module Test.Main where
 
 import Prelude
-import Control.Monad.Aff (Aff, forkAff, launchAff, joinFiber, attempt)
+
+import Control.Monad.Aff (Aff, attempt, forkAff, joinFiber, launchAff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Bus as Bus
 import Control.Monad.Aff.Console (log)
