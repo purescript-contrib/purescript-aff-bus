cb7960fd628ccc9b25acb0495b8c2301cc68f643
Irakli Safareli <i.safareli@gmail.com>
2018-11-26 18:00:25 +0400

    oops

--- src/Control/Monad/Aff/Bus.purs
+++ src/Control/Monad/Aff/Bus.purs
@@ -33,7 +33,7 @@ module Control.Monad.Aff.Bus
 import Prelude
 
 import Control.Monad.Aff (Aff, attempt, launchAff_)
-import Control.Monad.Aff.AVar (AVAR, AVar, isKilledVarm, killVar, makeEmptyVar, putVar, takeVar)
+import Control.Monad.Aff.AVar (AVAR, AVar, isKilledVar, killVar, makeEmptyVar, putVar, takeVar)
 import Control.Monad.Eff.AVar as EffAvar
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
 import Control.Monad.Eff.Exception as Exn
